bd3047517a153049f5f5451b05f83e111ee257da
Obamium Clock <103322298+ObamiumClock@users.noreply.github.com>
2025-12-03 19:16:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const txt = document.getElementById("text")
 const input = document.getElementById("input")
+const bg = document.body.style
+bg.backgroundColor = "black"
+txt.color = "white"
+
 
 function onclick() {
   txt.innerHTML = "You said: "+input.value 
+  console.log("msg submitted")
+  if (txt.innerHTML.includes(input.value)) {
+    console.log("msg success")
+  }
 }
