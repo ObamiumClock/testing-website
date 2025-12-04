e98501674b159d2f26e22601898396d6c65164aa
Obamium Clock <103322298+ObamiumClock@users.noreply.github.com>
2025-12-03 19:55:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-console.log("Script loaded!");
-
 const txt = document.getElementById("text")
 const input = document.getElementById("input")
 const title = document.getElementById("title")
-
 const bg = document.body.style
 bg.backgroundColor = "black"
 txt.style.color = "white"
 title.style.color = "white"
 
 const swears = ["fuck","bitch","retard","asshole","nigg","ass","shit","rape","fag","faggot"]
-let swearList = swears.length-1
 
 function updateTxt() {
   let containsSwear = false;
   
-  // Check if input contains any swear word
   for (let i = 0; i < swears.length; i++) {
     if (input.value.includes(swears[i])) {
       containsSwear = true;
-      break; // Stop checking once we find one
+      break;
     }
   }
   
