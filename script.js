ffd1f1588dc3599de132d92fc0be450a87ce54aa
Obamium Clock <103322298+ObamiumClock@users.noreply.github.com>
2025-12-03 19:31:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const txt = document.getElementById("text")
 const input = document.getElementById("input")
+const title = document.getElementById("title")
 const bg = document.body.style
 bg.backgroundColor = "black"
 txt.style.color = "white"
+title.style.color = "white"
 
 
 function onclick() {
